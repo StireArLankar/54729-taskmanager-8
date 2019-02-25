a3b82ff8d38b503d70f8a3ec356ab8bcdc2fe6dc
StireArLankar <stire.ru@gmail.com>
2019-02-25 21:56:11 +0300

    goforit

--- src/components/card/get-status-btns-template.js
+++ src/components/card/get-status-btns-template.js
@@ -1,10 +1,10 @@
-function statusBtnsTemplate() {
+const getStatusBtnsTemplate = () => {
   return `
   <div class="card__status-btns">
     <button class="card__save" type="submit">save</button>
     <button class="card__delete" type="button">delete</button>
   </div>
   `;
-}
+};
 
-export default statusBtnsTemplate;
+export default getStatusBtnsTemplate;
